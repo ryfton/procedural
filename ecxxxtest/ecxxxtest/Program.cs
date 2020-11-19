7edf59e28f82f904de2341beaaf3e7a52443f137
tarik <73602347+ryfton@users.noreply.github.com>
2020-11-19 10:15:48 +0100

    zede
    gfhfh

---
+++ ecxxxtest/ecxxxtest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ecxxxtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.MoveBufferArea("hqxjshxs");
+        }
+    }
+}
