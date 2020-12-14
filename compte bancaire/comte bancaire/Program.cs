3f45ead6f289ef06edf5602a80b7e7a9cbebc43b
tarik <73602347+ryfton@users.noreply.github.com>
2020-12-14 09:05:22 +0100

    le faite de ne pouvoire mettre des signe de poctuation dans le diagramme ,quand il est transformer en code il y a des erreur sur les attribut ect...

---
+++ compte bancaire/comte bancaire/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace comte_bancaire
+{
+    class Comp
+    {
+        private int decouvertAutoriserInt;
+        private int nomString;
+        private int numeroInt;
+        private int soldeInt;
+
+        static void Compte(string[] args)
+        {
+            
+            
+        }
+
+        public void CompteInt_numeroString_nomInt_soldeInt_decouvertAutaurise()
+        {
+            
+        }
+
+        public void CrediterInt_montantVoid()
+        {
+            
+        }
+
+        public void DebiterInt_montantBool()
+        {
+            
+        }
+
+        public void SuperieurCompte_autreCompteBool()
+        {
+            
+        }
+
+        public void TransfererInt_montantCompte_compteDestinataireBool()
+        {
+            
+        }
+
+        public void ToStringString()
+        {
+            
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Compte c = new Compte();
+            Compte c1 = new Compte(12345, "toto", 1000, -500);
+            Console.WriteLine(c.ToString());
+            Console.WriteLine(c1.ToString());
+        }
+
+    }
+
+    internal class Compte : Comp
+    {
+        public Compte()
+        {
+        }
+
+        public Compte(int v1, string v2, int v3, int v4)
+        {
+            V1 = v1;
+            V2 = v2;
+            V3 = v3;
+            V4 = v4;
+        }
+
+        public int V1 { get; }
+        public string V2 { get; }
+        public int V3 { get; }
+        public int V4 { get; }
+    }
+}
